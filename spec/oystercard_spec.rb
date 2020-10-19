8ca60d7e3c091c278746cd42d62b08ebcdebf4e6
Paula Gavenyte <paula.gavenyte@gmail.com>
2020-10-19 16:22:55 +0100

    ch4 done

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+
+  describe "#balance" do
+    it "oystercard shows balance" do 
+      card = Oystercard.new
+      expect(subject.balance).to eq 0
+    end
+  end
 end
